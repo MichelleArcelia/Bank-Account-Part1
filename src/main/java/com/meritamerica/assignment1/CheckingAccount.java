e6c02ec680dfdc3cded0756437517a2d5e8454f5
AllanaG <allanagray10@gmail.com>
2020-10-05 10:14:11 -0500

    Final Changes

--- src/main/java/com/meritamerica/assignment1/CheckingAccount.java
+++ src/main/java/com/meritamerica/assignment1/CheckingAccount.java
@@ -11,60 +11,83 @@ public class CheckingAccount extends MeritAmericaBankApp{
 	
 	
     // instance variable
-	private double balance;
+	private static double interestRate = 0.0001;
+	
+	double checkingBalance;
 	
 	
-	
-    /** Constructor for object of class CheckingAccount*/
+    //Constructor of objects of class CheckingAccount
 	public CheckingAccount(double openingBalance) {
-		balance = 100.00;
+		this.checkingBalance = openingBalance;
 	}
 
-	/** Get the balance, @return the balance*/
+	//Balance
 	public double getBalance() {
-		return balance;
+		return checkingBalance;
 	}
 	
 	
-	/** @return the interest rate*/
+	//Interest Rate
 	public double getInterestRate() {
-		double rate;
-		return rate = 0.001;
+		return interestRate;
 		 	
 		
 	}
 	
 
-    /** Remove money from the balance, @param amount the amount to withdraw
-    * @return true (success) or false (failure)*/
-	public boolean setwithdraw (double amount) {
-		if (balance - amount >= 0 ) {
-			balance -= amount;
-			return true;
-		}else {
+    //Withdraw
+	public boolean withdraw (double amount) {
+		if (checkingBalance < amount) {
+			System.out.println ("Sorry, there isn't enough money in your account");
 			return false;
+	}	
+		else { checkingBalance =checkingBalance - amount;
+		System.out.println ("Congrats take your withdrawn amount of:" + amount);
+		return true;
+		}
 	}
-	}		
 	
 	 
-	/** Add money to balance, @param amount the amount to deposit,* @void*/
-	public void setDeposit(double amount) {
-		balance = balance + amount;
+	//Deposit
+	public boolean deposit(double amount) {
+		if (amount < 0) {
+			System.out.println ("Sorry, your negative amount of " + amount + "can not be deposited");
+			return false;
 		
+		}
+		else { checkingBalance = checkingBalance + amount;
+		System.out.println ("Congrats we have received your deposit: " + amount);
+		
+		return true;
+		}
 	}
+		
+		
 	
+	// Future Value
 	public  double futureValue(int years) {
-		return years;
+		double FV = checkingBalance * Math.pow(1 + interestRate,years);
+		return FV;
 		
 		
 	}
 		
+	//StringtoStringOutput
+	public String toString() {
+		
+		return ("\n After your transaction here is your Checking Account in Detail: \n" +
+				"\n Checking Account Balance: " + getBalance()+
+				"\n Checking Account Interest Rate:" + getInterestRate()+
+				"\n Checking Account Balance in 3 years:" + futureValue(3)); 
+				
 	
+	}
 	
-	// public String toStrings(){
-	//	return null;
-	 }
+}
+	
+
 	 
+
 	
 	
 	
